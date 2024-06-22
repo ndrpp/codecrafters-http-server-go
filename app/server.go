9b4d5b8eb20b45c40f304f8596f02d7c32d5ad55
Andrei P <popandrei230@yahoo.com>
2024-06-22 15:09:44 +0300

    Add /files/{filename} API

--- app/server.go
+++ app/server.go
@@ -20,6 +20,25 @@ func handleConnection(c net.Conn) {
 	case request.URL.Path == "/":
 		_, err = c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
 
+	case strings.HasPrefix(request.URL.Path, "/files/"):
+		var dir string
+		if os.Args[1] == "--directory" {
+			dir = os.Args[2]
+		} else {
+			fmt.Fprintf(os.Stderr, "`--directory` flag is missing")
+			os.Exit(1)
+		}
+
+		filename := request.URL.Path[7:]
+		content, err := os.ReadFile(fmt.Sprintf("%s%s", dir, filename))
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Error reading file: %s\n", err)
+			_, err = c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+			break
+		}
+		data := string(content)
+		_, err = c.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s", len(data), data)))
+
 	case request.URL.Path == "/user-agent":
 		_, err = c.Write([]byte(fmt.Sprintf("HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s", len(request.UserAgent()), request.UserAgent())))
 
