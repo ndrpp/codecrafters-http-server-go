b1406f385639f0f7bac3389a0bdec454f887594b
Andrei P <popandrei230@yahoo.com>
2024-06-22 13:58:18 +0300

    Refactor request parsing

--- app/server.go
+++ app/server.go
@@ -1,12 +1,32 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"os"
-	"strings"
 )
+
+func handleConnection(c net.Conn) {
+	request, err := http.ReadRequest(bufio.NewReader(c))
+	if err != nil {
+		fmt.Println("Failed to read request", err.Error())
+	}
+
+	switch request.URL.Path {
+	case "/":
+		_, err = c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
+	default:
+		_, err = c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
+	}
+	if err != nil {
+		fmt.Println("Failed to write response to socket", err.Error())
+	}
+
+	c.Close()
+}
 
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
@@ -24,29 +44,7 @@ func main() {
 		}
 
 		go func(c net.Conn) {
-			fmt.Println("Connection from: ", c.RemoteAddr().String())
-
-			buffer := make([]byte, 1024)
-			_, err := c.Read(buffer)
-			if err != nil {
-				fmt.Println("Failed to read request", err.Error())
-			}
-			data := strings.Split(string(buffer), "\r\n")
-			path := data[0][4 : len(data[0])-9]
-
-			switch path {
-			case string("/"):
-				_, err = c.Write([]byte("HTTP/1.1 200 OK\r\n\r\n"))
-				if err != nil {
-					fmt.Println("Failed to write response to socket", err.Error())
-				}
-			default:
-				_, err = c.Write([]byte("HTTP/1.1 404 Not Found\r\n\r\n"))
-				if err != nil {
-					fmt.Println("Failed to write response to socket", err.Error())
-				}
-			}
-			c.Close()
+			handleConnection(c)
 		}(conn)
 	}
 }
